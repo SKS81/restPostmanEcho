d78315a79a3e859fa1c69c766c48e9cf114ffb63
Aleksandr Vedeneev <a-vedeneev@ya.ru>
2023-01-09 12:54:53 +0300

    Initial commit

---
+++ src/test/java/ru/netology/testPostmanEcho.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+
+class testPostmanEcho {
+
+    @Test
+    void shouldTestSomeData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Сделаем мир лучше!")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Сделаем мир лучше!"))
+        ;
+    }
+}
